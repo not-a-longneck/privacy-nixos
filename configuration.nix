23a753fa49441bf40b35e5e156db9caa7aa16435
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-04-04 01:10:48 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -122,6 +122,18 @@
   ];
 
   # ============================================================================
+  # DESKTOP ENVIRONMENT
+  # ============================================================================
+
+  # Enable the X11 windowing system
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+
+  # ============================================================================
   # SYSTEM VERSION
   # ============================================================================
 
