d86715eab8c80ca47dc6dfb4659735e9c8fbaf85
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-04-04 03:29:11 +0200

    Update flake.nix to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Privacy-focused NixOS with ephemeral root";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
