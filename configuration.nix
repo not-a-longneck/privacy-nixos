a754775d39d7db46fd1cee151bf0403ef6e1d606
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-04-04 02:40:40 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -112,7 +112,6 @@ imports = [
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   services.flatpak.enable = true;
-  services.spice-vdagentd.enable = true;   # This handles the system-level daemon
 
   environment.systemPackages = with pkgs; [
     git
@@ -126,11 +125,11 @@ imports = [
   # ============================================================================
 
   # Enable the X11 windowing system
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # ============================================================================
   # MOUNTS
