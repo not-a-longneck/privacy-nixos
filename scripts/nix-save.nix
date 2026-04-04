7c14b6d69d837ccdf7c6fb3d5a7c6be133c77cce
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-04-04 13:43:56 +0200

    backup hw config before update

--- scripts/nix-save.nix
+++ scripts/nix-save.nix
@@ -5,9 +5,18 @@ let
     CONFIG_DIR="/etc/nixos"
     cd $CONFIG_DIR
 
+    # 1. Protect the hardware file from the git reset
+    cp hardware-configuration.nix /tmp/hw-bak.nix
+    
+    # 2. Sync with GitHub
     sudo git fetch origin main
     sudo git reset --hard origin/main
-    sudo git add hardware-configuration.nix # ensure that hardware doesn't get deleted
+
+    # 3. Restore the hardware file
+    cp /tmp/hw-bak.nix hardware-configuration.nix
+    sudo git add hardware-configuration.nix
+
+    # 4. Rebuild the system
     sudo nix flake update
     sudo nixos-rebuild switch --flake .#privacy-vm
   '';
