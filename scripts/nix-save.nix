d72a329d2c31bf7e6b361d05cfacd1781789c818
not-a-longneck <118153658+not-a-longneck@users.noreply.github.com>
2026-04-04 03:36:05 +0200

    Update nix-save.nix

--- scripts/nix-save.nix
+++ scripts/nix-save.nix
@@ -7,28 +7,8 @@ let
     echo "1. Pulling latest changes from GitHub..."
     cd $CONFIG_DIR
     sudo git pull origin main --rebase
-
-    # This updates your lock file for new inputs like nix-flatpak!
-    echo "2. Updating dependencies..."
     sudo nix flake update
-
-    # echo "3. Staging changes..."
-    # sudo git add .
-
-    echo "4. Rebuilding the system..."
-    if sudo nixos-rebuild switch --flake "$CONFIG_DIR#privacy-vm"; then
-        
-        gen_num=$(readlink /nix/var/nix/profiles/system | cut -d- -f2)
-        
-        # echo "5. Saving and Syncing to GitHub..."
-        # sudo git commit -m "Gen $gen_num: Update via nix-save $(date +'%Y-%m-%d %H:%M')"
-        # sudo git push origin main
-        
-        echo "Successfully updated to Generation $gen_num and pushed to GitHub! 🎉"
-    else
-        echo "❌ Rebuild failed! Changes were not committed."
-        exit 1
-    fi
+    sudo nixos-rebuild switch --flake "$CONFIG_DIR#privacy-vm"
   '';
 in
 {
